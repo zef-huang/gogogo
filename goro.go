f753d24c1d4fbbad548f6c89b0e22b4d4744cb16
huangzefeng <737244264@qq.com>
2021-03-14 19:47:59 +0800

    协程体验

---
+++ goro.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+
+func goro_print(number string){
+	for i:=0; i<100; i++ {
+		fmt.Printf("I am %s goro\n", number)
+		time.Sleep(time.Millisecond)
+	}
+}
+
+
+// go 协程体验
+
+func main(){
+	go goro_print("1")
+	go goro_print("2")
+	go goro_print("3")
+
+	time.Sleep(time.Second)
+}
